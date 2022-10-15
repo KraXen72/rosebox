177d2277114440a09e3aabc88c1db7111dcdfa9b
KraXen72 <dpelech1@gmail.com>
2022-10-15 22:59:57 +0200

    discord: fix build script

--- discord/scripts/build.js
+++ discord/scripts/build.js
@@ -1,12 +1,11 @@
 const fs = require('fs')
 const colors = require('colors')
 
-const pathToOrig = __dirname.slice(0, __dirname.length - 7) + "dist\\main.css"
-const outputDir = `C:\\Users\\Nex\\AppData\\Roaming\\Discord\\settings.json\\rosebox.theme.css`
+const pathToSettingsJSON = `C:\\Users\\Nex\\AppData\\Roaming\\Discord\\settings.json`
 const importLink = `@import url('https://kraxen72.github.io/rosebox/discord/dist/main.css');`
 
-let contents = JSON.parse(fs.readFileSync(pathToOrig, 'utf8'))
+let contents = JSON.parse(fs.readFileSync(pathToSettingsJSON, 'utf8'))
 contents.openasar.css = importLink
-fs.writeFileSync(outputDir, JSON.stringify(contents))
+fs.writeFileSync(pathToSettingsJSON, JSON.stringify(contents))
 
 console.log(`sucessfully set openasar's css to '${importLink}'`.green)
