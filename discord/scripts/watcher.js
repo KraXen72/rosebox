78455c33737d7f3948dd37ed7f110d94366f3b2d
KraXen72 <dpelech1@gmail.com>
2022-10-22 14:44:34 +0200

    support vencord & add green + red classes

--- discord/scripts/watcher.js
+++ discord/scripts/watcher.js
@@ -4,7 +4,7 @@ const colors = require('colors')
 
 const dist = __dirname.slice(0, __dirname.length - 7) + "dist"
 const pathToOrig = `${dist}/main.css`
-const pathToSettingsJSON = `C:\\Users\\Nex\\AppData\\Roaming\\Discord\\settings.json`
+const MODE = "quickcss" // quickcss or settingsjson
 
 const wait = 500
 console.log(`${getTimestamp()}watching main.css`.cyan)
@@ -16,24 +16,35 @@ chokidar.watch(pathToOrig, { ignorePermissionErrors: true }).on('all', (event, p
 		console.clear();
 		console.log(`${getTimestamp()}changes detected in main.css`.yellow)
 
-		setTimeout(() => { //wait 'wait' amount of ms
-			let contents = fs.readFileSync(pathToOrig, 'utf8')
-			let settingsJSON = JSON.parse(fs.readFileSync(pathToSettingsJSON))
-			settingsJSON.openasar.css = contents.replaceAll('"', "\"")
-			fs.writeFileSync(pathToSettingsJSON, JSON.stringify(settingsJSON))
+		if (MODE === "settingsjson") {
+			setTimeout(() => { //wait 'wait' amount of ms
+				const pathToSettingsJSON = `C:\\Users\\Nex\\AppData\\Roaming\\Discord\\settings.json`
+				let contents = fs.readFileSync(pathToOrig, 'utf8')
+				let settingsJSON = JSON.parse(fs.readFileSync(pathToSettingsJSON))
+				settingsJSON.openasar.css = contents.replaceAll('"', "\"")
+				fs.writeFileSync(pathToSettingsJSON, JSON.stringify(settingsJSON))
+	
+				console.log(`${getTimestamp()}updated compiled.user.css`.green, `mode: ${MODE}`.cyan)
+			}, wait)
+		} else if (MODE === "quickcss") {
+			setTimeout(() => { //wait 'wait' amount of ms
+				const pathToQuickCSSFile = `C:\\Users\\Nex\\AppData\\Roaming\\Vencord\\settings\\quickCss.css`
 
-			console.log(`${getTimestamp()}updated compiled.user.css`.green)
-		}, wait)
-
+				let contents = fs.readFileSync(pathToOrig, 'utf8')
+				fs.writeFileSync(pathToQuickCSSFile, contents)
+	
+				console.log(`${getTimestamp()}updated compiled.user.css`.green, `mode: ${MODE}`.cyan)
+			}, wait)
+		}
 	}
-}).on('error', error => log(`Watcher error: ${error}`));
+}).on('error', error => console.log(`Watcher error: ${error}`));
 
 function getTimestamp() {
 	let d = new Date(Date.now())
 	return (`${pad(d.getHours(), 2)}:${pad(d.getMinutes(), 2)}:${pad(d.getSeconds(), 2)} > `)
 }
 function pad(n, width) {
-	z = '0';
+	let z = '0';
 	n = n + '';
 	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
