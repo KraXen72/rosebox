c8242dce0a8e94ddc0418ee8e5141d0ad0d92efd
KraXen72 <dpelech1@gmail.com>
2024-04-18 11:33:55 +0200

    discord: fix some visual glitches with reactions

--- discord/scripts/updateSelectors.js
+++ discord/scripts/updateSelectors.js
@@ -8,6 +8,8 @@ const banner = `// map placeholders to classes, so when discord changes the clas
 // unused mappings don't get included in the theme, due to the way they are written
 
 `
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 fetch("https://raw.githubusercontent.com/ClearVision/ClearVision-v6/master/lib/selectors/selectorPlaceholders.scss")
 	.then(data => data.text())
 	.then(text => {
